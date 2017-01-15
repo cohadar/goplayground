0b8aaa24c9746c74dfde628a90f3f8c5a763209e
Damir Cohadarevic <cohadar@gmail.com>
2017-01-15 11:42:09 +0100

    stupid docker

--- docker/gfetch/gfetch.go
+++ docker/gfetch/gfetch.go
@@ -1,23 +1,7 @@
 package main
 
-import (
-    "fmt"
-    "io/ioutil"
-    "net/http"
-    "os"
-)
+import "fmt"
 
 func main() {
-    resp, err := http.Get("https://google.com")
-    check(err)
-    body, err := ioutil.ReadAll(resp.Body)
-    check(err)
-    fmt.Println(len(body))
-}
-
-func check(err error) {
-    if err != nil {
-        fmt.Println(err)
-        os.Exit(1)
-    }
+	fmt.Println("Hello World!")
 }
